1a661a572d596c01fa5220d0ea9b09a5c5ca6b42
Lucas Russo <lerwys@gmail.com>
2019-09-18 13:19:07 -0300

    IA-20RaDiag01: remove docker swarm token

--- IA-20RaDiag01-CO-IOCSrv-1/boot-start-pre-container-apps.sh
+++ IA-20RaDiag01-CO-IOCSrv-1/boot-start-pre-container-apps.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Join Swarm
-docker swarm join --token SWMTKN-1-3ji2bbir1xg5i3totc57zjspbl87zvxxsuazn31f76vo11elao-d0ypso5uc9mhx7f80lfepf5d4 10.128.255.7:2377
+docker swarm join --token <DOCKER_SWARM_TOKEN> 10.128.255.7:2377
