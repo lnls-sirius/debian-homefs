b3e4a28dfecb5c848c929bcad1b61755c9e2017b
Lucas Russo <lerwys@gmail.com>
2019-09-18 13:17:45 -0300

    all: remove zabbix docker start from hook script
    This is done via docker swarm now.

--- IA-16RaBbB-CO-IOCSrv/boot-start-pre-container-apps.sh
+++ IA-16RaBbB-CO-IOCSrv/boot-start-pre-container-apps.sh
@@ -2,6 +2,3 @@
 
 # Join Swarm
 docker swarm join --token <DOCKER_SWARM_TOKEN> 10.128.255.7:2377
-
-# Init Zabbix Agent
-bash -c "cd ZabbixAgent && docker-compose -f ZabbixAgent.yml up -d"
--- IA-20RaDiag01-CO-IOCSrv-1/boot-start-pre-container-apps.sh
+++ IA-20RaDiag01-CO-IOCSrv-1/boot-start-pre-container-apps.sh
@@ -2,6 +2,3 @@
 
 # Join Swarm
 docker swarm join --token SWMTKN-1-3ji2bbir1xg5i3totc57zjspbl87zvxxsuazn31f76vo11elao-d0ypso5uc9mhx7f80lfepf5d4 10.128.255.7:2377
-
-# Init Zabbix Agent
-bash -c "cd ZabbixAgent && docker-compose -f ZabbixAgent.yml up -d"
